91ed4388ac18a3a0da95d83005bcef3ead4ca756
dev <dev@gmail.com>
2026-04-26 12:41:48 +0100

    Remove nested Invoices_Web duplicate directory

--- Invoices_Web/src/features/invoices/invoices.selectors.ts
+++
@@ -1,9 +0,0 @@
-import type { RootState } from '../../app/store';
-
-export const selectInvoicesState = (state: RootState) => state.invoices;
-export const selectInvoices = (state: RootState) => selectInvoicesState(state).items;
-export const selectInvoicesFetchStatus = (state: RootState) => selectInvoicesState(state).fetchStatus;
-export const selectInvoicesMutationStatus = (state: RootState) => selectInvoicesState(state).mutationStatus;
-export const selectSelectedInvoice = (state: RootState) => selectInvoicesState(state).selectedInvoice;
-export const selectInvoicesError = (state: RootState) => selectInvoicesState(state).error;
-
--- Invoices_Web/src/lib/api/index.ts
+++
@@ -1,4 +0,0 @@
-export { apiConfig } from './config';
-export { apiClient, get, patch, post, put, remove } from './client';
-export { ApiError, type ApiErrorDetails, normalizeApiError } from './errors';
-export type { ApiRequestConfig } from './client';
